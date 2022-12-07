3501154ccfdadcd5ff1eef29b72f88dbd982a35a
Darya <pavlovskaya3331@rambler.ru>
2022-12-07 13:50:30 +0300

    Решение здачи

--- Program.cs
+++ Program.cs
@@ -8,3 +8,37 @@
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → []
 */
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.Clear();
+Console.WriteLine("Программа, которая из имеющегося массива строк формирует массив из строк, длина которых либо меньше либо равна 3 символа");
+Console.WriteLine();
+Console.WriteLine("Введите колличество элементов ");
+int Volume = Convert.ToInt32(Console.ReadLine());
+string[] Array = new string[Volume];
+for (int i = 0; i < Volume; i++)
+{
+    Console.WriteLine($"Введите {i + 1}-е слово, цифру или символ: ");
+    string symbol = Console.ReadLine()!;
+    Array[i] = symbol;
+}
+string[] NewArray = new string[Volume];
+int length = 3;
+int count = 0;
+
+for (int i = 0; i < Volume; i++)
+{
+    if (Array[i].Length <= length)
+    {
+        NewArray[count] = Array[i];
+        count++;
+    }
+}
+Console.WriteLine();
+PrintArray(NewArray);
